6d61f2b7374029d4dae270d37fff8f226968e6b5
Liam <liam3006x@gmail.com>
2025-08-12 21:54:21 +0100

    Updates for legacy

--- config/tenancy.php
+++ config/tenancy.php
@@ -49,7 +49,7 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'tenant_',
+        'prefix' => 'lxst_legacy_tenant_',
         'suffix' => '',
 
         /**
