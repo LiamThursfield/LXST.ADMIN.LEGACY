f98aaf7572141f141970fe09ae7bdc2486426d7e
Liam <liam3006x@gmail.com>
2025-10-22 22:35:55 +0100

    Add nightwatch

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'nightwatch'],
             'ignore_exceptions' => false,
         ],
 
