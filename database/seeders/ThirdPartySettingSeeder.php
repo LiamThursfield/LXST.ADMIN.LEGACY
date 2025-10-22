355a8729e7a0c6ffd8f55e360b1254b65e0ab577
Liam <liam3006x@gmail.com>
2025-10-22 22:39:35 +0100

    remove stripe

--- database/seeders/ThirdPartySettingSeeder.php
+++ database/seeders/ThirdPartySettingSeeder.php
@@ -15,8 +15,8 @@ class ThirdPartySettingSeeder extends Seeder
     {
         $settings = app(\App\Models\Settings\ThirdPartySettings::class);
 
-        $settings->stripe_publishable_key = "pk_test_51NpzJ4IzVZalR528QleZng80HyvhCKF3yMlI2Wzcrhyc6Uz8FKXqul612Xh7FicWORVWiiBuvwJ3TUtWIn9SXho100rl3l7IUp";
-        $settings->stripe_secret_key = "sk_test_51NpzJ4IzVZalR528Lu9g7WLKnzWt32Hi9TDFMcBYftuubJup7PMyGC2sSpLLxB4Vydu61du13PdBzIBZ2xBb1Oxu00vxyTYuqT";
+        $settings->stripe_publishable_key = "";
+        $settings->stripe_secret_key = "";
 
         $settings->save();
     }
